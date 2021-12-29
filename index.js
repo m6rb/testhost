29497a2b2cb5503f403d6aa0575c0652efb12fd7
m6rb.dev <75396782+m6rb@users.noreply.github.com>
2021-12-29 22:07:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,4 +5,4 @@ const prefix = '+';
 client.on('ready',()=>{
     console.log(`Logged in As ${client.user.tag}`);
 });
- client.login('OTEzMzM0MTU4NDc3MTAzMTU0.YZ8-uA.mCfYCKvp6daq6RfRYRBl39F8810'); 
+ client.login(process.env.TOKEN); 
