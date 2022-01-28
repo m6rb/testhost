a9a7bf036cdd692effac66f07275c61299310c1a
m6rb.dev <75396782+m6rb@users.noreply.github.com>
2022-01-28 23:39:45 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const client = new Discord.Client();
 const prefix = '+';
 
 client.on('ready',()=>{
-    console.log(`Logged in As ${client.user.tag}`);
+    console.log(`${prefix}help`);
 });
  client.login('OTEzMzM0MTU4NDc3MTAzMTU0.YZ8-uA.5QrQsCQBBFJj-SRfOPqw_GpgXzo'); 
